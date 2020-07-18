46ef4187a1bec916426245bd13236cd9f60efdf4
Grant K <grantkveton@msn.com>
2020-07-17 23:51:40 -0400

    camera movement, and beginning of firing functionality

---
+++ udemy_fps_unity/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float hitPoints = 100;
+    
+    //TODO create public method to decrease hitpoints
+
+    public void TakeDamage(float damage)
+    {
+
+        hitPoints -= damage;
+
+        if(hitPoints <= 0)
+        {
+            hitPoints = 0;
+            Destroy(gameObject);
+            //TODO enemy death actions
+        }
+    }
+}
---
+++ udemy_fps_unity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera FPCamera;
+    [SerializeField] private float range = 100f;
+    [SerializeField] private float damage = 20f;
+    [SerializeField] private ParticleSystem flash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        PlayFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayFlash()
+    {
+        flash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("hit: " + hit.transform.name);
+            //TODO add hit effect
+            //Call method on EnemyHealth, to decrease enemy health
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+    }
+}

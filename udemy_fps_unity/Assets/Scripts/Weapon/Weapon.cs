9cd01fe891ad669fdf0fe65a2e3681814fef5a19
Grant K <grantkveton@msn.com>
2020-07-21 10:10:06 -0400

    main game mechanics implemented. level building process started

---
+++ udemy_fps_unity/Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera FPCamera;
+    [SerializeField] private float range = 100f;
+    [SerializeField] private float damage = 20f;
+    [SerializeField] private ParticleSystem flash;
+    [SerializeField] private GameObject hitEffect;
+    [SerializeField] private Ammo ammoSlot;
+    [SerializeField] private AmmoType ammoType;
+    [SerializeField] private float timeBetweenShots = 0.5f;
+
+    private bool canShoot = true;
+
+    private void OnEnable()
+    {
+        canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && canShoot)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+    
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        if (ammoSlot.GetCurrentAmount(ammoType) > 0)
+        {
+            ammoSlot.ReduceCurrentAmount(ammoType);
+            PlayFlash();
+            ProcessRaycast();
+        }
+        yield return new WaitForSeconds(timeBetweenShots);
+        canShoot = true;
+    }
+
+    private void PlayFlash()
+    {
+        flash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpact(hit);
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null || target.IsDead()) return;
+            target.TakeDamage(damage);
+        }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject effect = Instantiate(hitEffect, hit.transform.position, Quaternion.LookRotation(hit.normal));
+        Destroy(effect, 0.1f);
+    }
+}
